6df9c88d0c6f09c1f210d2e8593ce6ca06c85b21
Stepawka <stepan-lvov-04@mail.ru>
2025-08-28 20:45:12 +0300

    feat: Add font Roboto

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import { App } from "./app/app";
-import "@shared/config/i18n/i18next";
 import { Provider } from "react-redux";
 import { store } from "@store/store";
+import { App } from "./app/app";
+
+import "@shared/config/i18n/i18next";
+
+import "./index.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/600.css';
+import '@fontsource/roboto/700.css';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
