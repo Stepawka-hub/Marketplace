ec9291a84ee4441ae334e4173247135ea1faa82d
Stepawka <stepan-lvov-04@mail.ru>
2025-09-09 23:54:16 +0300

    refactor: Move ProductImages to containers

--- client/src/components/elements/product/index.ts
+++ client/src/components/elements/product/index.ts
@@ -4,5 +4,4 @@ export * from './product-meta';
 export * from './product-list';
 export * from './product-card';
 export * from './product-description';
-export * from './product-images';
 export * from './product-skeleton-list';
