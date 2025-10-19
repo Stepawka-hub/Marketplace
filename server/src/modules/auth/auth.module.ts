25b31d9397ebc28241e296b629cfdad6b9ba779c
Stepawka <stepan-lvov-04@mail.ru>
2025-10-20 00:26:05 +0300

    feat: set up jwt module

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '@/modules/user';
 import { UserEntity } from '@/modules/user/entities';
+import { getJwtConfig } from '@/config/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity]), UserModule],
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: getJwtConfig,
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([UserEntity]),
+    UserModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -3,16 +3,33 @@ import { Repository } from 'typeorm';
 import { RegisterRequestDto } from './dto/register.dto';
 import { UserEntity } from '../user/entities';
 import { InjectRepository } from '@nestjs/typeorm';
+import { hash } from 'argon2';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+  private readonly JWT_SECRET: string;
+  private readonly JWT_ACCESS_TOKEN_TTL: string;
+  private readonly JWT_REFRESH_TOKEN_TTL: string;
+
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
-  ) {}
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {
+    this.JWT_SECRET = configService.getOrThrow<string>('JWT_SECRET');
+    this.JWT_ACCESS_TOKEN_TTL = configService.getOrThrow<string>(
+      'JWT_ACCESS_TOKEN_TTL',
+    );
+    this.JWT_REFRESH_TOKEN_TTL = configService.getOrThrow<string>(
+      'JWT_REFRESH_TOKEN_TTL',
+    );
+  }
 
   async register(data: RegisterRequestDto) {
-    const { email, phone, firstName, lastName } = data;
+    const { email, phone, firstName, lastName, password } = data;
 
     const existUser = await this.userRepository.exists({ where: { email } });
 
@@ -27,6 +44,7 @@ export class AuthService {
       phone,
       firstName,
       lastName,
+      password: await hash(password),
     });
 
     await this.userRepository.save(user);
