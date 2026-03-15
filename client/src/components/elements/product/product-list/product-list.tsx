458bf9f03c9c5b42b6ea3a16d3e64ea7b1d784f9
Stepawka <stepan-lvov-04@mail.ru>
2026-03-16 01:56:35 +0300

    style: add pagination styles

--- client/src/components/elements/product/product-list/product-list.tsx
+++ client/src/components/elements/product/product-list/product-list.tsx
@@ -18,9 +18,18 @@ export const ProductListUI: FC<TProductListUIProps> = memo(
     }
 
     return (
-      <Grid container columnSpacing={2} rowSpacing={4}>
+      <Grid container columnSpacing={{ xs: 2, md: 2, lg: 2 }} rowSpacing={4}>
         {products.map((p) => (
-          <Grid key={p.id} size={{ xs: 12, sm: 6, lg: 4, xl: 2.4 }}>
+          <Grid
+            key={p.id}
+            size={{
+              xs: 12,
+              sm: 6,
+              md: 4,
+              lg: 3,
+              xl: 2.4,
+            }}
+          >
             <ProductCard product={p} />
           </Grid>
         ))}
