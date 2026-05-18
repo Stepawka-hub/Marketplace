ef25149ac0d2087fae537c1c9851cbb7ec77883d
Stepawka <stepan-lvov-04@mail.ru>
2026-05-18 03:13:59 +0300

    feat: add pollinginterval

--- client/src/components/containers/lot/lot-details/lot-details.tsx
+++ client/src/components/containers/lot/lot-details/lot-details.tsx
@@ -5,7 +5,9 @@ import { Loader } from "@/components/ui";
 import { TLotDetailsProps } from "./type";
 
 export const LotDetails: FC<TLotDetailsProps> = ({ lotId }) => {
-  const { data: lot, isLoading } = useGetLotByIdQuery(lotId);
+  const { data: lot, isLoading } = useGetLotByIdQuery(lotId, {
+    pollingInterval: 30000,
+  });
 
   if (isLoading) {
     return <Loader />;
